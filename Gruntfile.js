04ec21cc356f6bcf5724f91ca0f3fa3e8784b3b4
amandazummach <amanda_zummach@live.com>
2015-01-10 18:06:51 -0500

    Now using grunt vs gulp. Added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,26 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    jQuery: true
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true
+            },
+            files: ['<%= jshint.files %>', 'dist/index.html'],
+            tasks: []
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['watch']);
+
+};
